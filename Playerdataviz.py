b49fe623583bb9892867f01f9c23740733b19e84
Merwan <s.merwan@outlook.com>
2020-10-23 02:07:37 +0200

    graph curve & bar

--- Playerdataviz.py
+++ Playerdataviz.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 import numpy
 import csv
-from IPython.display import display, HTML
+#from IPython.display import display, HTML
 # from tabulate import tabulate
 import matplotlib.pyplot as plt
 
@@ -64,7 +64,7 @@ def bestPskill(season, skill):
 
 # graphs 
 
-def playerevolution(season, player, skill):
+def playerevolutionbar(season, player, skill):
     df20 = createdataframes(season)
     df19 = createdataframes(season-1)
     df18 = createdataframes(season-2)
@@ -78,14 +78,72 @@ def playerevolution(season, player, skill):
 
     df_evolution = pd.merge(df18new, df19new, on='short_name').merge(df20new, on='short_name')
     df_reduit = df_evolution[df_evolution['short_name']== str(player)][[str(skill)+"18", str(skill)+"19", str(skill)+"20"]]
-    
-    #.plt.pyplot.bar()
-    x = [18, 19, 20 ]
-    plt.bar(x,df_reduit.loc[0].values)
-    # print(df_reduit)
+   
+    x = [skill+" FIFA 18", skill+" FIFA 19", skill+" FIFA 20"]
+    y = df_reduit.iloc[0].values
+    plt.bar(x,y)
+    plt.title(player+" "+skill+' Evolution')
+    plt.show()
+
+
+def playerevolutioncurve(season, player, skill):
+    df20 = createdataframes(season)
+    df19 = createdataframes(season-1)
+    df18 = createdataframes(season-2)
+    listskilss = ["short_name","pace","shooting","passing","dribbling","defending","physic"]
+    df20new = df20[listskilss]
+    df19new = df19[listskilss]
+    df18new = df18[listskilss]
+    df18new.columns = ['short_name', 'pace18', 'shooting18', 'passing18', 'dribbling18', 'defending18','physic18']
+    df19new.columns = ['short_name', 'pace19', 'shooting19', 'passing19', 'dribbling19', 'defending19','physic19']
+    df20new.columns = ['short_name', 'pace20', 'shooting20', 'passing20', 'dribbling20', 'defending20','physic20']
+
+    df_evolution = pd.merge(df18new, df19new, on='short_name').merge(df20new, on='short_name')
+    df_reduit = df_evolution[df_evolution['short_name']== str(player)][[str(skill)+"18", str(skill)+"19", str(skill)+"20"]]
+   
+    x = [skill+" FIFA 18", skill+" FIFA 19", skill+" FIFA 20"]
+    y = df_reduit.iloc[0].values
+    plt.plot(x,y)
+    plt.title(player+" "+skill+' Evolution')
+    plt.show()
 
 
 
+def playerevolutionAllcurve(season, player):
+    df20 = createdataframes(season)
+    df19 = createdataframes(season-1)
+    df18 = createdataframes(season-2)
+    listskilss = ["short_name","pace","shooting","passing","dribbling","defending","physic"]
+    df20new = df20[listskilss]
+    df19new = df19[listskilss]
+    df18new = df18[listskilss]
+    df18new.columns = ['short_name', 'pace18', 'shooting18', 'passing18', 'dribbling18', 'defending18','physic18']
+    df19new.columns = ['short_name', 'pace19', 'shooting19', 'passing19', 'dribbling19', 'defending19','physic19']
+    df20new.columns = ['short_name', 'pace20', 'shooting20', 'passing20', 'dribbling20', 'defending20','physic20']
+
+    df_evolution = pd.merge(df18new, df19new, on='short_name').merge(df20new, on='short_name')
+    df_reduit = df_evolution[df_evolution['short_name']== str(player)]
+   
+    x = ["FIFA 20", "FIFA 19", "FIFA 18"]
+    ypace = df_reduit[["pace18", "pace19", "pace20"]].iloc[0].values
+    yshoot = df_reduit[["shooting18", "shooting19", "shooting20"]].iloc[0].values 
+    ypass = df_reduit[["passing18", "passing19", "passing20"]].iloc[0].values 
+    ydribl = df_reduit[["dribbling18", "dribbling19", "dribbling20"]].iloc[0].values 
+    ydefend = df_reduit[["defending18", "defending19", "defending20"]].iloc[0].values
+    yphys = df_reduit[["physic18", "physic19", "physic20"]].iloc[0].values
+
+    plt.plot(x,ypace)
+    plt.plot(x,yshoot)
+    plt.plot(x,ypass)
+    plt.plot(x,ydribl)
+    plt.plot(x,ydefend)
+    plt.plot(x,yphys)
+    plt.title(player+' skills Evolution')
+    plt.grid(True)
+    plt.show()
+
+
+playerevolutionAllcurve(20, "M. Özil")
 
 # import matplotlib.pyplot as plt
 # fig = plt.figure()
@@ -97,7 +155,6 @@ def playerevolution(season, player, skill):
 
 
 
-# playerevolution(20, "R. Lewandowski", "dribbling")
 
 
 # "short_name","pace","shooting","passing","dribbling","defending","physic"
@@ -108,17 +165,25 @@ def playerevolution(season, player, skill):
 
 
 
-def topclubByskill(skill, top):
-    df = createdataframes(20)
-    dfnew = df[["nationality", str(skill)]]
+# def topclubByskill(skill, top):
+#     df = createdataframes(20)
+#     mask = ["FC Barcelona", "Juventus", "Paris Saint-Germain", "Atlético Madrid", "Real Madrid", "Manchester City", "Liverpool",
+#             "Napoli", "Tottenham Hotspur", "Manchester United", "Chelsea", "FC Bayern München", "Inter", "Borussia Dortmund", "Arsenal",
+#             "Valencia CF", "AS Monaco", "FC Girondins de Bordeaux", "SL Benfica", "Olympique de Marseille", "Lokomotiv Moscow",
+#             "Galatasaray SK", "Sevilla FC", "Roma", "Sporting CP"]
+    
+#     df_mask = df["club"] in mask
+#     df[df[df_mask]]
+#     dfnew = df[["club", str(skill)]]
 
-    dfcalc = dfnew.groupby("nationality")[str(skill)].mean()
-    # dfcalc.rename[]
-    dfcalc.columns = ['test', 'Moyenne']
+#     dfcalc = dfnew.groupby("club")[str(skill)].mean()
+#     # dfcalc.rename[]
+#     dfcalc = pd.DataFrame(dfcalc)<
+#     # dfcalc.columns = ['National team', 'avg']
+#     print(df_mask)
+#     print(dfcalc.sort_values(str(skill), ascending=False).head(top))
+#     #print(dfcalc)
 
-    # print(dfcalc.sort_values(by='Moyenne', ascending=False).head(top))
-    print(dfcalc)
 
-
-topclubByskill("pace", 5)
+# topclubByskill("physic", 10)
 
